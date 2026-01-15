20505c15f6c4080aabf4d45245fad62ef5498446
Chiicake <pankaiwen1231@outlook.com>
2026-01-15 09:19:51 +0800

    value

--- kv-core/src/storage_struct/value.rs
+++ kv-core/src/storage_struct/value.rs
@@ -1,9 +1,17 @@
 use std::collections::{HashMap, HashSet, BTreeMap};
+use std::time::Instant;
+
 #[derive(Debug, Clone)]
-pub enum Value {
+pub enum ValueData {
     String(Vec<u8>),
     List(Vec<Vec<u8>>),
     Hash(HashMap<Vec<u8>, Vec<u8>>),
     Set(HashSet<Vec<u8>>),
     ZSet(BTreeMap<f64, HashSet<Vec<u8>>>),
+}
+
+#[derive(Debug, Clone)]
+pub struct Value {
+    data: ValueData,
+    expire_at: Option<Instant>,
 }
