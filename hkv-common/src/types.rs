a88a5ab8df7093fda96760daa1ca905b020a4889
Chiicake <pankaiwen1231@outlook.com>
2026-01-17 09:20:27 +0800

    feat: add Key struct

--- hkv-common/src/types.rs
+++ hkv-common/src/types.rs
@@ -1,2 +1,24 @@
 pub const MAX_KEY_SIZE: usize = 256;
 pub const MAX_VALUE_SIZE: usize = 1024;
+
+#[repr(C)]
+#[derive(Clone, Hash, PartialEq, Debug)]
+pub struct Key {
+    len: u16,
+    data: [u8; MAX_KEY_SIZE],
+}
+
+impl Key {
+    pub fn new(data: &[u8]) -> Option<Self> {
+        if data.len() > MAX_KEY_SIZE {
+            return None;
+        }
+
+        let mut key = Key {
+            len: data.len() as u16,
+            data: [0u8; MAX_KEY_SIZE],
+        };
+        key.data[..data.len()].copy_from_slice(data);
+        Some(key)
+    }
+}
